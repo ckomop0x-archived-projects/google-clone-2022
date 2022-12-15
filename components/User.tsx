2d135fea536f78b91fbe59663efd1181eb94a456
Pavel Klochkov <paul.klochkov@gmail.com>
2022-12-15 19:22:10 +0100

    Add auth and get user data from Google

--- components/User.tsx
+++ components/User.tsx
@@ -1,3 +1,34 @@
-const User = () => <div>User</div>;
+import { signIn, signOut, useSession } from 'next-auth/react';
+import Image from 'next/image';
+import { MouseEventHandler } from 'react';
+
+const User = () => {
+  const { data: session } = useSession();
+
+  const onSignIn = () => void signIn();
+
+  const onSignOut = () => void signOut();
+
+  if (session) {
+    return (
+      <button onClick={onSignOut}>
+        <img
+          src={session.user?.image || ''}
+          alt=""
+          className="h-10 w-10 rounded-full p-1 hover:bg-gray-200"
+        />
+      </button>
+    );
+  }
+
+  return (
+    <button
+      onClick={onSignIn}
+      className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+    >
+      Sign in
+    </button>
+  );
+};
 
 export default User;
---
+++ pages/auth/signin.tsx
@@ -0,0 +1,54 @@
+import { NextPage } from 'next';
+import { BuiltInProviderType } from 'next-auth/providers';
+import {
+  ClientSafeProvider,
+  getProviders,
+  LiteralUnion,
+  signIn,
+} from 'next-auth/react';
+
+import Header from '../../components/Header';
+
+interface SignInPageProps {
+  providers: Promise<Record<
+    LiteralUnion<BuiltInProviderType, string>,
+    ClientSafeProvider
+  > | null>;
+}
+const SignInPage: NextPage<SignInPageProps> = ({ providers }) => (
+  <>
+    <Header />
+    <div className="mt-40">
+      {Object.values(providers).map(provider => (
+        <div key={provider.name} className="flex flex-col items-center">
+          <img
+            className="w-52 object-cover"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+            alt="Google logo"
+          />
+          <p className="text-sm italic my-10 text-center">
+            This website is created for learning purposes
+          </p>
+          <button
+            className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
+            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+          >
+            Sing in with {provider.name}
+          </button>
+        </div>
+      ))}
+    </div>
+  </>
+);
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
+
+export default SignInPage;
